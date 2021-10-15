a246162fcf868583289c64841194cf133e457515
NICOTYAN <fumomoSan@outlook.jp>
2021-10-15 16:12:23 +0900

    勝敗の判定

---
+++ Assets/Script/BlackJack.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlackJack : MonoBehaviour
+{
+    [SerializeField] Text judgement;
+    [SerializeField] Text score;
+    [SerializeField] Text life;
+
+    [SerializeField] GameObject pCard;
+    [SerializeField] Text pNumber;
+    [SerializeField] GameObject eCard;
+    [SerializeField] Text eNumber;
+
+    int nowScore = 0;
+    int nowLife = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DealCards();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nowLife == 0)
+        {
+            judgement.text = "GAMEOVER";
+        }
+    }
+    public void DealCards()
+    {
+        int pScore = 0;
+        int eScore = 0;
+        
+        pScore = Random.Range(1, 14);
+        pNumber.text = pScore.ToString();
+        eScore = Random.Range(1, 14);
+        eNumber.text = eScore.ToString();
+        if (pScore > eScore)
+        {
+            nowScore++;
+            score.text = "Score:" + nowScore.ToString();
+            judgement.text = "WIN";
+        }
+        else if (pScore < eScore)
+        {
+            judgement.text = "LOSE";
+            nowLife--;
+            life.text = "Life:" + nowLife;
+        }
+        else 
+        {
+            judgement.text = "DRAW";
+        }
+    }
+    public void Again()
+    {
+        DealCards();
+    }
+}
